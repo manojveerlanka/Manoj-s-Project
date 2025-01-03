96a407464b3715b4f14fc1e3b7c24687f7273aea
manoj veerlanka <manojveerlanka70@gmail.com>
2025-01-03 07:11:04 -0600

    New changes

--- Angular/FirstAngularApp/src/app/app.component.ts
+++ Angular/FirstAngularApp/src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CoursesComponent } from './courses/courses.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CoursesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'FirstAngularApp';
+  description = 'This is a simple Angular application';
 }
